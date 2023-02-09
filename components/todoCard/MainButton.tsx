e168245258c4b3d7c0b07e8dba0e9e46c78e6031
Abdul_Hanan <abdulhanan3279@gmail.com>
2023-02-09 16:57:25 +0500

    main button props

--- components/todoCard/MainButton.tsx
+++ components/todoCard/MainButton.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react"
 import react from "react"
 import { Button } from '@chakra-ui/react'
 import { AddIcon } from "@chakra-ui/icons"
-const MainButton = (props) => {
+const MainButton = (props:any) => {
     return (
         <Button  colorScheme='blue' onClick={props.onClick} >{props.icon}</Button>
     )
